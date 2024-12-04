281b7f8b8af6440a443a493f86dcb2af367ce998
Vladimir <v.kuznetsov@wonny.net>
2024-12-04 14:57:40 +0300

    Updated dependencies for Starscream fork branch

--- Package.swift
+++ Package.swift
@@ -11,27 +11,45 @@ let package = Package(
         .tvOS(.v10)
     ],
     products: [
-        .library(name: "CocoaMQTT", targets: [ "CocoaMQTT" ]),
-        .library(name: "CocoaMQTTWebSocket", targets: [ "CocoaMQTTWebSocket" ])
+        .library(
+            name: "CocoaMQTT",
+            targets: [ "CocoaMQTT" ]
+        ),
+        .library(
+            name: "CocoaMQTTWebSocket",
+            targets: [ "CocoaMQTTWebSocket" ]
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/daltoniam/Starscream.git", .exact("4.0.8")),
-        .package(url: "https://github.com/leeway1208/MqttCocoaAsyncSocket", from: "1.0.8"),
+        .package(
+            url: "https://github.com/MalbyApps/Starscream_daltoniam",
+            .branch("malby")
+        ),
+        .package(
+            url: "https://github.com/leeway1208/MqttCocoaAsyncSocket",
+            from: "1.0.8"
+        ),
     ],
     targets: [
-        .target(name: "CocoaMQTT",
-                dependencies: [ "MqttCocoaAsyncSocket" ],
-                path: "Source",
-                exclude: ["CocoaMQTTWebSocket.swift"],
-                swiftSettings: [ .define("IS_SWIFT_PACKAGE")]),
-        .target(name: "CocoaMQTTWebSocket",
-                dependencies: [ "CocoaMQTT", "Starscream" ],
-                path: "Source",
-                sources: ["CocoaMQTTWebSocket.swift"],
-                swiftSettings: [ .define("IS_SWIFT_PACKAGE")]),
-        .testTarget(name: "CocoaMQTTTests",
-                    dependencies: [ "CocoaMQTT", "CocoaMQTTWebSocket" ],
-                    path: "CocoaMQTTTests",
-                    swiftSettings: [ .define("IS_SWIFT_PACKAGE")])
+        .target(
+            name: "CocoaMQTT",
+            dependencies: [ "MqttCocoaAsyncSocket" ],
+            path: "Source",
+            exclude: ["CocoaMQTTWebSocket.swift"],
+            swiftSettings: [ .define("IS_SWIFT_PACKAGE")]
+        ),
+        .target(
+            name: "CocoaMQTTWebSocket",
+            dependencies: [ "CocoaMQTT", "Starscream" ],
+            path: "Source",
+            sources: ["CocoaMQTTWebSocket.swift"],
+            swiftSettings: [ .define("IS_SWIFT_PACKAGE")]
+        ),
+        .testTarget(
+            name: "CocoaMQTTTests",
+            dependencies: [ "CocoaMQTT", "CocoaMQTTWebSocket" ],
+            path: "CocoaMQTTTests",
+            swiftSettings: [ .define("IS_SWIFT_PACKAGE")]
+        )
     ]
 )
